dbd98e95ddd17f35d4e6888c9a88964c6d1a730f
Mike McGuire <MoonshotMike@gmail.com>
2016-06-07 14:36:45 -0400

    working on property binding unit

--- src/app/data-binding.component.ts
+++ src/app/data-binding.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {PropertyBindingComponent} from './property-binding.component'
+
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'data-binding.component.html',
-  styleUrls: ['data-binding.component.css']
+  styleUrls: ['data-binding.component.css'],
+  template: `
+    <my-property-binding></my-property-binding>
+        `,
+  directives: [PropertyBindingComponent]
 })
 export class MyAppComponent {
-  title = 'My Udemy really still works!';
 }
---
+++ src/app/property-binding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'my-property-binding',
+  template: `
+    <h2>This is the child component</h2>
+`
+})
+
+export class PropertyBindingComponent {
+
+}
